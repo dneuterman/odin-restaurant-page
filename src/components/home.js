b108e03f61248826232ec6bd81e44820e7ddee44
Donald Neuterman <dneuterman@pop-os.localdomain>
2022-05-07 20:52:28 -0400

    added page switching functionality

---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+export default class HomePage {
+  constructor(header) {
+    this.header = header;
+  }
+
+  get header() {
+    return this._header;
+  }
+
+  set header(value) {
+    this._header = value;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-alert("webpack works!");
+import HomePage from "./components/home";
+import MenuPage from "./components/menu";
+import AboutPage from "./components/about";
+
+const content = document.getElementById("content");
+const homeNav = document.querySelector(".home-nav");
+const menuNav = document.querySelector(".menu-nav");
+const aboutNav = document.querySelector(".about-nav");
+const homePageDisplay = new HomePage("My Restaurant");
+const menuPageDisplay = new MenuPage("Restaurant Menu");
+const aboutPageDisplay = new AboutPage("About Us");
+
+const switchPage = (page) => {
+  content.textContent = page.header;
+}
+
+homeNav.addEventListener("click", () => {
+  switchPage(homePageDisplay);
+});
+
+menuNav.addEventListener("click", () => {
+  switchPage(menuPageDisplay);
+});
+
+aboutNav.addEventListener("click", () => {
+  switchPage(aboutPageDisplay);
+});
