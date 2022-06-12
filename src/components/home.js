a06e527956c40fd25d39fe99010fa91f384d7b32
Donald Neuterman <dneuterman@pop-os.localdomain>
2022-06-11 22:21:47 -0400

    updated home page functionality

--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,35 @@
 export default class HomePage {
-  constructor(header) {
-    this.header = header;
+  constructor({restaurantTitle, restaurantSubTitle, restaurantDescription}) {
+    this.restaurantTitle = restaurantTitle;
+    this.restaurantSubTitle = restaurantSubTitle;
+    this.restaurantDescription = restaurantDescription;
   }
 
-  get header() {
-    return this._header;
-  }
+  buildPage() {
+    const homePageContainer = document.createElement('div');
 
-  set header(value) {
-    this._header = value;
+    const restaurantTitleContainer = document.createElement('div');
+    restaurantTitleContainer.classList.add("restaurant-title-container");
+
+    const restaurantTitle = document.createElement('h1');
+    restaurantTitle.classList.add('restaurant-title');
+    restaurantTitle.textContent = this.restaurantTitle;
+
+    const restaurantSubTitle = document.createElement('h2');
+    restaurantSubTitle.classList.add('restaurant-sub-title');
+    restaurantSubTitle.textContent = this.restaurantSubTitle;
+
+    const restaurantDescriptionContainer = document.createElement('div');
+    restaurantDescriptionContainer.classList.add('restaurant-description-container');
+
+    const restaurantDescription = document.createElement('h3');
+    restaurantDescription.classList.add('restaurant-description');
+    restaurantDescription.textContent = this.restaurantDescription;
+
+    restaurantTitleContainer.append(restaurantTitle, restaurantSubTitle);
+    restaurantDescriptionContainer.append(restaurantDescription);
+    homePageContainer.append(restaurantTitleContainer, restaurantDescriptionContainer);
+
+    return homePageContainer;
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,26 +3,32 @@ import MenuPage from "./components/menu";
 import AboutPage from "./components/about";
 import "./style.css";
 
+const HOME_PAGE_OBJECT = {
+  restaurantTitle: "My Restaurant",
+  restaurantSubTitle: "Here to Serve You!",
+  restaurantDescription: "We started as a single-shop in the corner of the mall and we have grown into a world-renowned restaurant with over 4000 stores! You won't find food like this any where elese! Come in and enjoy!"
+}
+
 const content = document.getElementById("content");
 const homeNav = document.querySelector(".home-nav");
 const menuNav = document.querySelector(".menu-nav");
 const aboutNav = document.querySelector(".about-nav");
-const homePageDisplay = new HomePage("My Restaurant");
+const homePageDisplay = new HomePage(HOME_PAGE_OBJECT);
 const menuPageDisplay = new MenuPage("Restaurant Menu");
 const aboutPageDisplay = new AboutPage("About Us");
 
 const switchPage = (page) => {
-  content.textContent = page.header;
+  content.append(page.buildPage());
 }
 
 homeNav.addEventListener("click", () => {
   switchPage(homePageDisplay);
 });
 
-menuNav.addEventListener("click", () => {
-  switchPage(menuPageDisplay);
-});
+// menuNav.addEventListener("click", () => {
+//   switchPage(menuPageDisplay);
+// });
 
-aboutNav.addEventListener("click", () => {
-  switchPage(aboutPageDisplay);
-});
+// aboutNav.addEventListener("click", () => {
+//   switchPage(aboutPageDisplay);
+// });
